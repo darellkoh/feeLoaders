3faa56e699b413ae95fc0feffeb77f28d0ac409f
jbosman <joseph.a.bosman@gmail.com>
2016-09-13 15:56:13 -0400

    Update products.js
    Update with Milad, Joe, and Lilly.

--- server/db/models/products.js
+++ server/db/models/products.js
@@ -32,6 +32,6 @@ module.exports = db.define('product', {
     },
     photo: {
         type: Sequelize.STRING,
-        allowNull: false
+        defaultValue: 'http://image.spreadshirtmedia.com/image-server/v1/compositions/19354318/views/1,width=280,height=280,appearanceId=2,version=1440417743.png/lightning-cat_design.png',
     }
 });
