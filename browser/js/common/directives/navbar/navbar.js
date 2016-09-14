2585d6c3eb7b7fb36e8b33f801e74018a019e938
Stephanie Harris <310srh@gmail.com>
2016-09-14 16:36:06 -0400

    added bs wrapper

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -8,8 +8,8 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
 
             scope.items = [
                 { label: 'Home', state: 'home' },
-                { label: 'About', state: 'about' },
-                { label: 'Documentation', state: 'docs' },
+                { label: 'Products', state: 'about' },
+                { label: 'FAQ', state: 'docs' },
                 { label: 'Members Only', state: 'membersOnly', auth: true }
             ];
 
--- server/app/configure/static-middleware.js
+++ server/app/configure/static-middleware.js
@@ -10,10 +10,13 @@ module.exports = function (app) {
     var npmPath = path.join(root, './node_modules');
     var publicPath = path.join(root, './public');
     var browserPath = path.join(root, './browser');
+    var imgPath = path.join(root, './img');
 
     app.use(favicon(app.getValue('faviconPath')));
     app.use(express.static(npmPath));
     app.use(express.static(publicPath));
     app.use(express.static(browserPath));
+    app.use(express.static(imgPath));
 
 };
+
