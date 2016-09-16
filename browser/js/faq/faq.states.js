ddbaaa76af071d2e0d5806ced5a84a3d55ad86d0
Stephanie Harris <310srh@gmail.com>
2016-09-16 18:43:32 -0400

    created faq template & state

--- browser/js/faq/faq.states.js
+++ browser/js/faq/faq.states.js
@@ -1,10 +1,10 @@
 app.config(function($stateProvider){
   $stateProvider.state('faq', {
     url: '/faq',
-    templateUrl: '/js/faq/faq.html',
-    controller: function($scope){
-      $scope.collapse =
-      $scope.panel
-    }
+    templateUrl: '/js/faq/faq.html'
+    // controller: function($scope){
+    //   $scope.collapse =
+    //   $scope.panel
+    // }
   })
 });
