43152c28b25ff2ec746afd0f595038d6c9d1dd74
Joe Bosman <joseph.a.bosman@gmail.com>
2016-09-20 18:52:52 -0400

    Filled in more details on order details view

--- browser/js/Order/orderDetails/orderDetails.controller.js
+++ browser/js/Order/orderDetails/orderDetails.controller.js
@@ -2,8 +2,18 @@ app.controller( 'orderDetailsCtrl', function($scope, $stateParams, orderDetailsF
 
 	orderDetailsFactory.getOrderById($stateParams.id)
 	.then(function(order){
+
 		$scope.order = order;
 		$scope.orderItems = order.orderItems;
+
+		$scope.orderTotal = function(){
+			return $scope.orderItems.reduce( function(prev, curr){
+				prev += curr.productCost;
+				return prev;
+			}, 0);
+		}
+
+
 	});
 	
 });
